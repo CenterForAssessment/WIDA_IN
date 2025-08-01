e2ecbe19f75b8eef1256b48b25731d9058edb048
Damian Betebenner <dbetebenner@nciea.org>
2025-08-01 10:23:03 -0400

    Adding in 2025 scripts

---
+++ WIDA_IN_Data_LONG_2025.R
@@ -0,0 +1,73 @@
+#################################################################################
+###
+### Data preparation script for WIDA IN data 2025
+###
+#################################################################################
+
+### Load Packages
+
+require(SGP)
+require(data.table)
+
+
+### Utility function
+
+strtail <- function (s, n = 1) {
+    if (n < 0)
+        substring(s, 1 - n)
+    else substring(s, nchar(s) - n + 1)
+}
+
+strhead <- function (s, n) {
+    if (n < 0)
+        substr(s, 1, nchar(s) + n)
+    else substr(s, 1, n)
+}
+
+
+### Load Data
+WIDA_IN_Data_LONG_2025 <- fread("Data/Base_Files/WIDA_ACCESS_IN_2024_2025.csv")
+
+
+### Clean Up Data
+WIDA_IN_Data_LONG_2025[,STN:=NULL]
+WIDA_IN_Data_LONG_2025[,SCHOOL_YEAR_ID:=NULL]
+old.names <- c("IDOE_CORPORATION_ID", "IDOE_SCHOOL_ID", "STUDENT_ID", "GRADE", "Composite_Overall_Scale Score", "Composite_Overall_Proficiency Level")
+setnames(WIDA_IN_Data_LONG_2025, c("DISTRICT_NUMBER", "SCHOOL_NUMBER", "ID", "GRADE", "SCALE_SCORE", "ACHIEVEMENT_LEVEL_ORIGINAL"))
+WIDA_IN_Data_LONG_2025[,YEAR := "2025"]
+WIDA_IN_Data_LONG_2025[,ID := as.character(ID)]
+WIDA_IN_Data_LONG_2025[GRADE=="KG", GRADE:="00"]
+WIDA_IN_Data_LONG_2025[,GRADE:=as.character(as.numeric(GRADE))]
+WIDA_IN_Data_LONG_2025[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+WIDA_IN_Data_LONG_2025[,ACHIEVEMENT_LEVEL := as.character(ACHIEVEMENT_LEVEL_ORIGINAL)]
+WIDA_IN_Data_LONG_2025[,ACHIEVEMENT_LEVEL := strhead(ACHIEVEMENT_LEVEL, 1)]
+WIDA_IN_Data_LONG_2025[ACHIEVEMENT_LEVEL_ORIGINAL %in% c("4.2", "4.3", "4.4"), ACHIEVEMENT_LEVEL:="4.2"]
+WIDA_IN_Data_LONG_2025[ACHIEVEMENT_LEVEL_ORIGINAL %in% c("4.5", "4.6", "4.7", "4.8", "4.9"), ACHIEVEMENT_LEVEL:="4.5"]
+WIDA_IN_Data_LONG_2025[!is.na(ACHIEVEMENT_LEVEL),ACHIEVEMENT_LEVEL := paste("WIDA Level", ACHIEVEMENT_LEVEL)]
+WIDA_IN_Data_LONG_2025[,VALID_CASE := "VALID_CASE"]
+WIDA_IN_Data_LONG_2025[,CONTENT_AREA := "READING"]
+
+
+### Check for duplicates
+
+setkey(WIDA_IN_Data_LONG_2025, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID, SCALE_SCORE)
+setkey(WIDA_IN_Data_LONG_2025, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
+WIDA_IN_Data_LONG_2025[which(duplicated(WIDA_IN_Data_LONG_2025, by=key(WIDA_IN_Data_LONG_2025)))-1, VALID_CASE := "INVALID_CASE"]
+setkey(WIDA_IN_Data_LONG_2025, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
+
+### Invalidate data with missing scale scores
+
+WIDA_IN_Data_LONG_2025[is.na(SCALE_SCORE), VALID_CASE:="INVALID_CASE"]
+
+### Reorder
+
+setcolorder(WIDA_IN_Data_LONG_2025, c(9, 10, 7, 4, 3, 5, 8, 6, 1, 2))
+
+
+### INVALIDATE cases
+
+WIDA_IN_Data_LONG_2025[is.na(GRADE), VALID_CASE:="INVALID_CASE"]
+
+### Save data
+
+save(WIDA_IN_Data_LONG_2025, file="Data/WIDA_IN_Data_LONG_2025.Rdata")
